20b198983b218d9941d032e957fbdf99386795aa
Adan <adanschiffer@hotmail.com>
2024-08-05 19:34:33 -0600

    Preentrega 01: Producto final

--- preentrega_01/README.md
+++ preentrega_01/README.md
@@ -17,7 +17,8 @@ REDSHIFT_PORT=$YOUR_REDSHIFT_PORT
 
 4. Ejecuta el script de Python que llena la tabla `news` con datos.
 ```bash
-python main.py --clean-results-table
+python main.py --clean-results-table --date=YYYY-MM-DD
 ```
-
 __[!]__ La bandera `clean-results-table` hace que la tabla `news` se trunque antes de ingresar datos, resultando en una tabla que unicamente contiene los datos recíen extraidos con cada ejecución. Se recomienda eliminar esta bandera si el script escribe en una tabla final (utilizable desde otro lado).
+
+__[!]__ La bandera `date` es necesaria para ejecutar el script, de esta forma podemos controlar los datos que leemos e ingresamos a la tabla `news`. El formato utilizado es ISO 8601 y solo acepta `date`, no `datetime`.
--- preentrega_01/main.py
+++ preentrega_01/main.py
@@ -2,7 +2,14 @@ from modules.extract import extract as _extract
 from modules.transform import transform as _transform
 from modules.load import load as _load
 from dotenv import load_dotenv
+import re
 import argparse
+
+def is_date_iso_8601(date_str):
+	ISO_8601_DATE_PATTERN = r'^\d{4}-\d{2}-\d{2}$'
+	if re.match(ISO_8601_DATE_PATTERN, date_str):
+		return True
+	return False
 
 def main():
 	load_dotenv()
@@ -11,12 +18,21 @@ def main():
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--clean-results-table", action="store_true")
+	parser.add_argument("--date", type=str)
 	args = parser.parse_args()
 	clean_results_table = args.clean_results_table
 
 	topic = "Cryptocurrency"
-	start_date = "2024-08-01T00:00:00.000"
-	end_date = "2024-08-01T23:59:59.999"
+
+	date: str = args.date
+	start_date = f"{date}T00:00:00.000"
+	end_date = f"{date}T23:59:59.999"
+
+	if not date:
+		raise ValueError("Date not entered, use the flag --date=<ISO_8061_DATE>")
+
+	if not is_date_iso_8601(date):
+		raise ValueError(f"Date entered '{date}' is not on format ISO 8601")
 
 	_extract(topic, start_date, end_date, out_data_stg_json)
 	_transform(out_data_stg_json, out_data_csv)
